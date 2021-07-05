c6097fe489aa6ebb5f210211b117437d8f2a2858
LeeGod <lee20040919@gmail.com>
2021-07-05 18:53:24 +0700

    Set @Verified annotated and AbstractPlugin as bean classes

--- src/main/java/org/fairy/intellij/FairyIntelliJ.java
+++ src/main/java/org/fairy/intellij/FairyIntelliJ.java
@@ -1,7 +1,12 @@
 package org.fairy.intellij;
 
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiType;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiTypesUtil;
 import lombok.experimental.UtilityClass;
 import org.fairy.intellij.constant.ClassConstants;
@@ -12,16 +17,26 @@ public class FairyIntelliJ {
     public static final String DEFAULT_WRAPPER_VERSION = "7.1";
 
     public boolean isBean(PsiType type) {
+        final Project project = ProjectManager.getInstance().getOpenProjects()[0];
+        final JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
+        final PsiClass abstractPlugin = psiFacade.findClass(ClassConstants.ABSTRACT_PLUGIN_CLASS, GlobalSearchScope.allScope(project));
+
         final PsiClass psiClass = PsiTypesUtil.getPsiClass(type);
         if (psiClass == null) {
             return false;
         }
-        return psiClass.hasAnnotation(ClassConstants.COMPONENT_CLASS) || psiClass.hasAnnotation(ClassConstants.SERVICE_CLASS);
+
+        if (abstractPlugin != null && psiClass.isInheritor(abstractPlugin, true)) {
+            return true;
+        }
+
+        // check every inherit class for Verified Classes
+        return psiClass.hasAnnotation(ClassConstants.VERIFIED_CLASS) || psiClass.hasAnnotation(ClassConstants.COMPONENT_CLASS) || psiClass.hasAnnotation(ClassConstants.SERVICE_CLASS);
     }
 
     public String getLatestFrameworkVersion() {
         // TODO - Cloud fetch
-        return "0.4b1";
+        return "0.4b2";
     }
 
 }
--- src/main/java/org/fairy/intellij/constant/ClassConstants.java
+++ src/main/java/org/fairy/intellij/constant/ClassConstants.java
@@ -8,6 +8,8 @@ public class ClassConstants {
     public final String AUTOWIRED_CLASS = "org.fairy.bean.Autowired";
     public final String SERVICE_CLASS = "org.fairy.bean.Service";
     public final String COMPONENT_CLASS = "org.fairy.bean.Component";
+    public final String VERIFIED_CLASS = "org.fairy.bean.Verified";
+    public final String ABSTRACT_PLUGIN_CLASS = "org.fairy.plugin.AbstractPlugin";
     public final String CONFIGURATION_ELEMENT_CLASS = "org.fairy.config.annotation.ConfigurationElement";
     public final String YAML_CONFIGURATION_CLASS = "org.fairy.config.yaml.YamlConfiguration";
     public final String ENTITY_CLASS = "javax.persistence.Entity";
